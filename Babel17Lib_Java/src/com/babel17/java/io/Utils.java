f07393506ca80a775b8fa87fbc2c0f8fca7e3609
Steven Obua <steven.obua@googlemail.com>
2011-08-11 03:32:29 +0200

    fixes

--- Babel17Lib_Java/src/com/babel17/java/io/Utils.java
+++ Babel17Lib_Java/src/com/babel17/java/io/Utils.java
@@ -1,9 +1,30 @@
 package com.babel17.java.io;
 
+import java.io.IOException;
+import java.io.Reader;
+
 public class Utils {
   
   public static String fromChar(char c) {
     return String.valueOf(c);
+  }
+  
+  public static String readLine(Reader r) throws IOException {
+    StringBuffer buf = new StringBuffer();
+    int prevc = -1;
+    do {
+      int c = r.read();
+      if (c < 0) {
+        if (prevc == -1) return null;
+        else return buf.toString();
+      } else if (c == '\n') {
+        if (prevc == '\r') buf.deleteCharAt(buf.length()-1);
+        return buf.toString();
+      } else {
+        buf.append((char) c);
+        prevc = c;
+      }
+    } while (true);
   }
   
 }
